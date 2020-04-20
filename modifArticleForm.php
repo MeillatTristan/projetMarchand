84166c09ba6b68e8963491704000f78345df2453
Alex Beurthe <alexbeurthebasket1@orange.fr>
2020-04-20 15:11:20 +0200

    page modif articles

---
+++ modifArticleForm.php
@@ -0,0 +1,85 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <title>Daisy Website Template | Home :: w3layouts</title>
+  <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="js/jquery.min.js"></script>
+  <!-- Custom Theme files -->
+  <link href="css/style.css" rel='stylesheet' type='text/css' />
+
+  <!---- start-smoth-scrolling---->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!----//End-top-nav-script---->
+
+</head>
+
+<body>
+  <?php
+  session_start();
+  ?>
+  <div class="containerAdminAll">
+    <?php include "sidebarAdmin.php" ?>
+    <?php
+    include "configbdd.php";
+    if (!isset($_SESSION['id'])) {
+      header("Location : index.php");
+    }
+    ?>
+    <div class="containerAdmin">
+      <?php include "header.php" ?>
+      <style type="text/css">
+        .bg {
+          background: #eceef2
+        }
+      </style>
+
+      <!---- header-info ---->
+      <div class="header-info text-center">
+        <?php
+        $requete = $bdd->query('SELECT * FROM articles');
+        ?>
+        <h3>Utilisateurs :</h3>
+        <div style='overflow-x:auto;'>
+          <table>
+            <thead>
+              <tr class="row">
+                <th>Nom de l'article</th>
+                <th>Fruit/Légume</th>
+                <th>Quantité</th>
+                <th>Prix</th>
+                <th>Provenace</th>
+                <th>En vente</th>
+                <th>Photo</th>
+              </tr>
+            </thead>
+            <tbody class='tableau'>
+              <?php
+              
+              while ( $articles = $requete->fetch()){
+              echo "<tr>";
+              
+                echo "<td>". $articles['name']."</td>" ;
+                echo "<td>". $articles['type']."</td>" ;
+                echo "<td>". $articles['quantity']."</td>" ;
+                echo "<td>". $articles['prix']."</td>" ;
+                echo "<td>". $articles['provenance']."</td>" ;
+                echo "<td>". $articles['venteBool'] . "</td>";
+                echo "<td><img src='images/".$articles['picture'] ."' alt='photo de l'article'></td>";
+                // echo "<td><a href='profil.php?idMaj=".$articles[' id']."'>Modifier</a></td>" ;
+                echo "</tr>";
+              }
+              ?>
+              
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+</body>
+
+</html>
