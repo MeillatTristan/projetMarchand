3d33d062d6089f6cea72fc10c87b7a741dea9d0d
Alex Beurthe <alexbeurthebasket1@orange.fr>
2020-04-21 16:32:37 +0200

    upgrade panel admin

---
+++ modifArticle.php
@@ -0,0 +1,8 @@
+<?php
+$name = $_REQUEST["name"] ;
+$type = $_REQUEST["type"] ;
+$quantity = $_REQUEST["quantity"] ;
+$prix = $_REQUEST["prix"] ;
+$provenance = $_REQUEST["provenance"] ;
+$vente = $_REQUEST["VenteBool"] ;
+?>
