835355d987164b17557398af586f7685ada7c06e
Alex Beurthe <alexbeurthebasket1@orange.fr>
2020-04-27 16:43:26 +0200

    changement validuser

--- inscription.php
+++ inscription.php
@@ -19,12 +19,13 @@
 <body>
 
 <div><form action='valid_user.php'>
-  <div><input name='firstname' id='firstname'  required placeholder='Nom' > </input></div>
-  <div><input name='lastname' id='lastname'  required placeholder='Prénom' > </input></div>       
-  <div><input name='email' id='email'  required placeholder='Email' > </input></div> 
-  <div><input name='adresse' id='adresse'  required placeholder='Adresse' > </input></div> 
-  <div><input name='valid_mdp1' id='password_1'  required placeholder='Mot de passe' > </input></div> 
-  <div><input name='valid_mdp2' id='password_2'  required placeholder='Confirmation de mot de passe' > </input></div> 
-  <div><button type='submit' name='valid_user'>Valider</button></div>
+  <input name='firstname' id='firstname'  required placeholder='Nom' > </input>
+  <input name='lastname' id='lastname'  required placeholder='Prénom' > </input>       
+  <input name='email' id='email'  required placeholder='Email' > </input> 
+  <input name='adresse' id='adresse'  required placeholder='Adresse' > </input> 
+  <input name='telephone' id='telephone'  required placeholder='Numéros de téléphone' > </input> 
+  <input name='valid_mdp1' id='password_1'  required placeholder='Mot de passe' > </input> 
+  <input name='valid_mdp2' id='password_2'  required placeholder='Confirmation de mot de passe' > </input> 
+  <button type='submit' name='valid_user'>Valider</button>
 </form>
 </div>
--- valid_user.php
+++ valid_user.php
@@ -1,13 +1,15 @@
 <?php
 include 'configbdd.php' ;
+
 $firstname = $_REQUEST["firstname"] ;
 $lastname = $_REQUEST["lastname"] ;
 $email = $_REQUEST["email"] ;
 $adresse = $_REQUEST["adresse"] ;
-$password_1 = $_REQUEST["password_1"] ;
-$password_2 = $_REQUEST["password_2"] ;
+$telephone = $_REQUEST["telephone"];
+$password_1 = $_REQUEST["valid_mdp1"] ;
 
-$query = $bdd->prepare("UPDATE user SET firstname=:firstname, lastname=:lastname, email=:email, adresse=:adresse, password_1=:password_1, password_2=:password_2 WHERE id=:id");
-$query->execute(['firstname'=>$firstname, 'lastname'=>$lastname, 'email'=>$email, 'adresse'=>$adresse, 'password_1'=>$password_1, 'password_2'=>$password_2, 'id'=>$id]) ;
+
+$query = $bdd->prepare("INSERT INTO users SET firstname=:firstname, lastname=:lastname, email=:email, adresse=:adresse, telephone=:telephone, password_1=:password_1 ");
+$query->execute(['firstname'=>$firstname, 'lastname'=>$lastname, 'email'=>$email, 'adresse'=>$adresse,'telephone'=>$telephone, 'password_1'=>$password_1 ]) ;
 //header('Location: http://localhost/projetMarchand/index.php');
 //exit();
