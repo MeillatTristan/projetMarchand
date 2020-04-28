50c61d6b4e2eddaba8d62c92797dcb1f7fe6f46f
elfepee <tristan.meillat28@gmail.com>
2020-04-28 17:47:20 +0100

    bdd en ligne

--- delArticlePanier.php
+++ delArticlePanier.php
@@ -12,3 +12,16 @@ if (empty($_SESSION['panier'])){
 header("Location:panier.php")
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
