8f8ad28b211875ed75147def2598454c66a72bbf
Alex Beurthe <alexbeurthebasket1@orange.fr>
2020-06-29 16:26:14 +0200

    page accueil

--- index.php
+++ index.php
@@ -1,112 +1,113 @@
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<title>Daisy Website Template | Home :: w3layouts</title>
-		<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="js/jquery.min.js"></script>
-		 <!-- Custom Theme files -->
-		<link href="css/style.css" rel='stylesheet' type='text/css' />
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
 
-   		 <!-- Custom Theme files -->
-   		  <!---- start-smoth-scrolling---->
-		<script type="text/javascript" src="js/move-top.js"></script>
-		<script type="text/javascript" src="js/easing.js"></script>
-		<script type="text/javascript">
-			jQuery(document).ready(function($) {
-				$(".scroll").click(function(event){		
-					event.preventDefault();
-					$('html,body').animate({scrollTop:$(this.hash).offset().top},1500);
-				});
+<head>
+	<title>Daisy Website Template | Home :: w3layouts</title>
+	<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery.min.js"></script>
+	<!-- Custom Theme files -->
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+
+	<!-- Custom Theme files -->
+	<!---- start-smoth-scrolling---->
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event) {
+				event.preventDefault();
+				$('html,body').animate({
+					scrollTop: $(this.hash).offset().top
+				}, 1500);
 			});
-		</script>
-		 <!---- start-smoth-scrolling---->
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-		</script>
-		<!----start-top-nav-script---->
-		<script>
-			$(function() {
-				var pull 		= $('#pull');
-					menu 		= $('nav ul');
-					menuHeight	= menu.height();
-				$(pull).on('click', function(e) {
-					e.preventDefault();
-					menu.slideToggle();
-				});
-				$(window).resize(function(){
-	        		var w = $(window).width();
-	        		if(w > 320 && menu.is(':hidden')) {
-	        			menu.removeAttr('style');
-	        		}
-	    		});
+		});
+	</script>
+	<!---- start-smoth-scrolling---->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script type="application/x-javascript">
+		addEventListener("load", function() {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	</script>
+	<!----start-top-nav-script---->
+	<script>
+		$(function() {
+			var pull = $('#pull');
+			menu = $('nav ul');
+			menuHeight = menu.height();
+			$(pull).on('click', function(e) {
+				e.preventDefault();
+				menu.slideToggle();
 			});
-		</script>
-		<!----//End-top-nav-script---->
+			$(window).resize(function() {
+				var w = $(window).width();
+				if (w > 320 && menu.is(':hidden')) {
+					menu.removeAttr('style');
+				}
+			});
+		});
+	</script>
+	<!----//End-top-nav-script---->
 
-	</head>
-	<body id='body'>
-		<?php
-		session_start();
-		include 'sidebar.php'
-		?>
+</head>
 
-		<div id="top" class="bg">
+<body id='body'>
+	<?php
+	session_start();
+	include 'sidebar.php'
+	?>
+
+	<div id="top" class="bg">
 		<!----- start-header---->
-			<div id="home" class="header">
-					<div class="top-header">
-						<div class="container">
-						<!----start-top-nav---->
-						<?php include "header.php" ?>
-					</div>
-				</div>
-			</div>
-
-			<!---- header-info ---->
-			<div class="header-info indexPage text-center">
-				<div class="container title">
-					<h1><span> </span><label>SALAH </label> PRIMEURS<span> </span></h1>
-					<p>Votre marchand</p>
-				</div>
-			</div>
-			</div>
-			<div class="clearfix"> </div>
-			<!---- header-info ---->
-			<!--- about-us ---->
-			<div id="about" class="about">
+		<div id="home" class="header">
+			<div class="top-header">
 				<div class="container">
-					<div class="about-head text-center">
-            <h3><span>A propos</span></h3>
-            <p>Daisy is sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum</p>
-					</div>
-
-			<!---- Address ---->
-			<div id="contact" class="address">
-				<div class="address-info text-center">
-					<h3>Adresse</h3>
-					<p>615 4th Ave S, Jacksonville Beach, FL 32250</p>
+					<!----start-top-nav---->
+					<?php include "header.php" ?>
 				</div>
-				<div class="map">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d177504.0179213423!2d-0.5970969089488695!3d47.51826750012542!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48087127748b35f3%3A0x40d37521e0d8610!2sVillev%C3%AAque%2C%2049140%20Rives-du-Loir-en-Anjou!5e0!3m2!1sfr!2sfr!4v1587214424827!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>				</div>
 			</div>
-			<!---- Address ---->
-			<!---- footer ---->
-			<script type="text/javascript">
-                                    $(document).ready(function() {
-                                        /*
-                                        var defaults = {
-                                              containerID: 'toTop', // fading element id
-                                            containerHoverID: 'toTopHover', // fading element hover id
-                                            scrollSpeed: 1200,
-                                            easingType: 'linear' 
-                                         };
-                                        */
+		</div>
 
-                                        $().UItoTop({ easingType: 'easeOutCubic' });
+		<!---- header-info ---->
+		<div class="header-info indexPage text-center">
+			<div class="container title">
+				<h1><span> </span><label>SALAH </label> PRIMEURS<span> </span></h1>
+				<p id="marchand">Votre marchand</p>
+				<p id="primeur">Votre primeur sélectionne pour vous les meilleurs fruits et légumes</p>
+			</div>
+		</div>	
+	</div>
+			<div class="clearfix"> </div>
+			<!--- Information ---->
+			<div class="information">
+				<div class="info">
+					<h2>Nos services</h2>
+					<p id="textinfo">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia doloremque vero rerum numquam minus dolore vitae accusantium eligendi incidunt provident! Assumenda nulla in eos eveniet temporibus autem? Quidem, eligendi modi.</p>
+				</div>
+				<div class="info">
+					<h2>Les commandes</h2>
+					<p id="textinfo">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia doloremque vero rerum numquam minus dolore vitae accusantium eligendi incidunt provident! Assumenda nulla in eos eveniet temporibus autem? Quidem, eligendi modi.</p>
+				</div>
+				<div class="info">
+					<h2>Marchés</h2>
+					<p id="textinfo">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia doloremque vero rerum numquam minus dolore vitae accusantium eligendi incidunt provident! Assumenda nulla in eos eveniet temporibus autem? Quidem, eligendi modi.</p>
+				</div>
+				<div class="infoimage">
+					<img src="" alt="">
+				</div>
+			</div>	
+			<!---- Map ---->
+			<div class="map">
+				<h2>Voici la carte, les livraisons a domicile ce passe exclusivement dans ces zones</h2>
+				<img src="images/map.png" alt="">
+			</div>
+</body>
 
-                                    });
-                                </script>
-                <a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
-	</body>
 </html>
