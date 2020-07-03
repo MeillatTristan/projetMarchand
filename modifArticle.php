20e1e3b2f5a8447910839d9bdd6107aa35f4b63b
elfepee <tristan.meillat28@gmail.com>
2020-07-03 15:21:58 +0100

    bug modif article

--- modifArticle.php
+++ modifArticle.php
@@ -10,6 +10,9 @@ $provenance = $_REQUEST['provenance'];
 if(isset($_FILES['picture']['name'])){
   $namePicture = $_FILES['picture']['name'];
   move_uploaded_file($_FILES['picture']['tmp_name'], "images/" . $namePicture);
+  $queryInsert = $bdd->prepare("UPDATE articles SET picture = :picture WHERE id = :id");
+  $queryInsert->execute(array(':picture' => $namePicture, ':id'=>$id));
+
 }
 
 $description = $_REQUEST['description'];
@@ -22,8 +25,8 @@ else{
   $bio = 'n';
 }
 
-$queryInsert = $bdd->prepare("UPDATE articles SET name = :name, prix = :price, picture = :picture, venteBool= :venteBool, provenance= :provenance, description = :description, type= :type, bio = :bio WHERE id = :id");
-$queryInsert->execute(array(':name' =>$name, ':price'=>$price, ':picture' => $namePicture, ':venteBool' =>'y', ':provenance'=>$provenance, ':description'=>$description, ':type'=>$type, ':bio'=>$bio, ':id'=>$id));
+$queryInsert = $bdd->prepare("UPDATE articles SET name = :name, prix = :price, venteBool= :venteBool, provenance= :provenance, description = :description, type= :type, bio = :bio WHERE id = :id");
+$queryInsert->execute(array(':name' =>$name, ':price'=>$price, ':venteBool' =>'y', ':provenance'=>$provenance, ':description'=>$description, ':type'=>$type, ':bio'=>$bio, ':id'=>$id));
 
 header('Location:modifArticleList.php');
 ?>
